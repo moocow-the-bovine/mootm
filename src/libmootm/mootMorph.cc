885940d3d9030bec94215031affd9df1ea635563
Bryan Jurish <jurish@bbaw.de>
2004-08-10 22:51:05 +0000

    + added COPYING.LIB, lgpl-ified library
    git-svn-id: file:///home/svn/dev/mootm/trunk@326 4a515de8-beb3-11dc-84d6-2b654028afac

--- src/libmootm/mootMorph.cc
+++ src/libmootm/mootMorph.cc
@@ -4,19 +4,19 @@
    libmootm : moocow's morphology library
    Copyright (C) 2003-2004 by Bryan Jurish <moocow@ling.uni-potsdam.de>
 
-   This program is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 2 of the License, or
-   (at your option) any later version.
-
-   This program is distributed in the hope that it will be useful,
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+   
+   This library is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+   
+   You should have received a copy of the GNU Lesser General Public
+   License along with this library; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 */
 
 
@@ -38,14 +38,12 @@
 #include <set>
 #include <list>
 
-#include <mootToken.h>
+#include <mootMorph.h>
 #include <mootTokenIO.h>
 
 #if FSM_API_REVISION > 0
 # include <fstream>
 #endif /* FSM_API_REVISION */
-
-#include "mootMorph.h"
 
 namespace mootm {
   using namespace std;
--- src/libmootm/mootMorph.h
+++ src/libmootm/mootMorph.h
@@ -4,19 +4,19 @@
    libmootm : moocow's morphology library
    Copyright (C) 2003-2004 by Bryan Jurish <moocow@ling.uni-potsdam.de>
 
-   This program is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 2 of the License, or
-   (at your option) any later version.
-
-   This program is distributed in the hope that it will be useful,
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+   
+   This library is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+   
+   You should have received a copy of the GNU Lesser General Public
+   License along with this library; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 */
 
 
@@ -36,12 +36,15 @@
 #define moot_MORPH_DEBUG
 //#undef moot_MORPH_DEBUG
 
+/** \def MOOT_HAVE_HASH_STRING
+ *  Hack to use libFSM hash<string> from libFSM */
+#define MOOT_HAVE_HASH_STRING
+
 #include <stdio.h>
+#include <mootFSM.h>
 #include <mootToken.h>
 #include <mootTokenIO.h>
 
-#include "mootFSM.h"
-/* #include <mootTypes.h> */
 
 namespace mootm {
 
